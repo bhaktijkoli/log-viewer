b5795f4b6d104d57979f1c56d3f8e4b302e5e735
Bhaktij Koli <bhaktijkoli121@gmail.com>
2022-07-20 18:04:30 +0530

    feat: added list api

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 require('dotenv').config()
+const fs = require('fs')
 const express = require('express')
 const app = express()
 const port = process.env.PORT
+const logDir = process.env.LOG_DIR
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.get('/list', (_, res) => {
+    const files = fs.readdirSync(logDir)
+    res.status(200).json(files)
 })
 
 app.listen(port, () => {
