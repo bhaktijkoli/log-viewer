a5bb3eda4ef2db3b4cfd4b3d1753c7849f5a3779
Bhaktij Koli <bhaktijkoli121@gmail.com>
2022-07-20 18:09:03 +0530

    feat: added view api

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const fs = require('fs')
+const path = require('path')
 const express = require('express')
 const app = express()
 const port = process.env.PORT
@@ -8,6 +9,16 @@ const logDir = process.env.LOG_DIR
 app.get('/list', (_, res) => {
     const files = fs.readdirSync(logDir)
     res.status(200).json(files)
+})
+
+app.get('/view/:file', (req, res) => {
+    const file = req.params.file
+    const filePath = path.join(logDir, file)
+    if (fs.existsSync(filePath) === false) {
+        res.sendStatus(404)
+        return
+    }
+    res.sendFile(filePath)
 })
 
 app.listen(port, () => {
